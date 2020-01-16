385be3e7a6395eadfab64b79054a4e1b2be1f041
BrianWu <brianwu291@gmail.com>
2020-01-16 23:37:05 +0800

    refs #master, refact it to be more clear

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function findFirstDuplicate(nums = []) {
   if (nums.length <= 1) return -1
-  const mapObj = {}, result = []
-  for (let i = 0; i < nums.length && result.length === 0; i += 1) {
+  const mapObj = {}
+  for (let i = 0; i < nums.length; i += 1) {
     const currentNum = nums[i]
     if (mapObj[currentNum] === undefined) {
       mapObj[currentNum] = 1
     } else {
-      result.push(currentNum)
+      return currentNum
     }
   }
-  return result.length === 0 ? - 1 : result[0]
+  return -1
 }
 
 module.exports = {
