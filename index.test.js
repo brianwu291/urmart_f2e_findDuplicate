8ea564f0cd77f236d61bf1a8ed5722f719e619f9
BrianWu <brianwu291@gmail.com>
2020-01-17 11:32:13 +0800

    refs #master, fix the typo on case3 test description

--- index.test.js
+++ index.test.js
@@ -13,6 +13,6 @@ it('should return minus one when input array is [ 2, 4, 5, 1, 3]', () => {
   expect(findFirstDuplicate([ 2, 4, 5, 1, 3])).toBe(-1)
 })
 
-it('should return minus one when input array is [ 2, 1, 3, 5, 3, 2] ', () => {
+it('should return three when input array is [ 2, 1, 3, 5, 3, 2] ', () => {
   expect(findFirstDuplicate([ 2, 1, 3, 5, 3, 2] )).toBe(3)
 })
