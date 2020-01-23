b194d57f17052f9ece6d3f9bb2072ca3f0f00719
BrianWu <brianwu291@gmail.com>
2020-01-24 02:30:09 +0800

    refs #master, to test leetcode 565

--- index.js
+++ index.js
@@ -11,7 +11,37 @@ function findFirstDuplicate(nums = []) {
   }
   return -1
 }
+function arrayNesting(nums) {
+  function getSetLengthOfIndex(index = 0) {
+    const mapObj = {}
+    let result = 0, lastVal = 0, currentVal = 0, isRepeated = false
+    while (!isRepeated) {
+      if (result === 0) {
+        result += 1
+        mapObj[nums[index]] = 1
+        lastVal = nums[index]
+      }
+      currentVal = nums[lastVal]
+      if (mapObj[currentVal] === undefined && currentVal !== undefined) {
+        result += 1
+        mapObj[currentVal] = 1
+        nums[lastVal] = 'visited'
+        lastVal = currentVal
+      } else {
+        isRepeated = true
+      }
+    }
+    return result
+  }
+  if (nums.length <= 1) return nums.length
+  return nums.reduce((pre, num, index) => {
+    if (num === 'visited') return pre
+    const temp = getSetLengthOfIndex(index)
+    return Math.max(temp, pre)
+  }, 0)
+}
 
 module.exports = {
-  findFirstDuplicate
+  findFirstDuplicate,
+  arrayNesting,
 }
